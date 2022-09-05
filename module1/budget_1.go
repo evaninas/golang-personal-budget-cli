d355cc4ff4b38dbb5406edb7fe85533cc553df16
Edgaras Vaninas <e.vaninas@gmail.com>
2022-09-05 20:13:34 +0300

    Completed module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Max float32
+}
 
 // Item stores item information
+type Item struct {
+	Items       []Item
+	Description string
+	Price       float32
+}
